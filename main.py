09f6f4f6d911f2866ea7d6f40f50a7d7c281575e
Abinash <avinashniure026@gmail.com>
2022-03-11 20:01:17 +0930

    final

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from tkinter import *
 
+exchange_rate = 90
+
+def convert():
+    value_in_Aud = float(aud_box.get())
+    value_in_Nrp = float(aud_box.get())
+    if value_in_Aud != 0.0:
+        value_in_Nrp = value_in_Aud * exchange_rate
+        nrp_box.delete(0, END)
+        nrp_box.insert(0, f"{value_in_Nrp}")
+    elif value_in_Nrp != 0.0:
+        value_in_Aud = value_in_Nrp / exchange_rate
+        aud_box.delete(0, END)
+        aud_box.insert(0, f"{value_in_Aud}")
+
+
+
 window = Tk()
 window.title("Unit changing")
 window.minsize()
@@ -15,20 +31,16 @@ value_in_Aud = 0
 value_in_Nrp = 0
 
 aud_box = Entry()
-aud_box.insert(END, string=f"{value_in_Aud}")
 aud_box.focus()
-value_in_Aud = aud_box.get()
 aud_box.grid(column=0, row=2)
 
 arrow_label = Label(text="⇋", font=("Arial", 20))
 arrow_label.grid(column=1, row=2)
 
 nrp_box = Entry()
-nrp_box.insert(END, string=f"{value_in_Nrp}")
-value_in_Nrp = aud_box.get()
 nrp_box.grid(column=2, row=2)
 
-con_button = Button(text="Convert")
+con_button = Button(text="Convert", command=convert)
 con_button.grid(column=1, row=3)
 
 window.mainloop()
